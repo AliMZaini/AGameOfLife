86b65eeb65ed8845df56cc5452372a1574ae32d1
Ali Z <51179189+AliMZaini@users.noreply.github.com>
2020-04-26 21:50:47 +0100

    Updates

---
+++ precache-manifest.b65e49f48fa0a3c6fd6f20fbd200fa4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e43314cdc4b7dc05286624304180d84",
+    "url": "/AGameOfLife/index.html"
+  },
+  {
+    "revision": "a1340b63ccc7bb8309e9",
+    "url": "/AGameOfLife/static/css/main.d819b42d.chunk.css"
+  },
+  {
+    "revision": "d97681b6a02bc7ecac83",
+    "url": "/AGameOfLife/static/js/2.ac1c5c53.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/AGameOfLife/static/js/2.ac1c5c53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1340b63ccc7bb8309e9",
+    "url": "/AGameOfLife/static/js/main.7b1a1c1a.chunk.js"
+  },
+  {
+    "revision": "784bb49f6562b6363e2a",
+    "url": "/AGameOfLife/static/js/runtime-main.a4f76c93.js"
+  }
+]);
